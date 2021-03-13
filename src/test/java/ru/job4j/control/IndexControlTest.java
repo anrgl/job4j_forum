4223dfba38dd4749873f664fa74bed90ed6e1740
Aynur Galimov <galimovaynur@protonmail.com>
2021-03-13 14:14:37 +0500

    0. Spring test [#450483]

---
+++ src/test/java/ru/job4j/control/IndexControlTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.Main;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+public class IndexControlTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void shouldReturnLogin() throws Exception {
+        this.mockMvc.perform(get("/login"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    @Test
+    public void shouldReturnReg() throws Exception {
+        this.mockMvc.perform(get("/reg"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("reg"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnPost() throws Exception {
+        this.mockMvc.perform(get("/post?id=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnNewPost() throws Exception {
+        this.mockMvc.perform(get("/new"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("new"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnEditPost() throws Exception {
+        this.mockMvc.perform(get("/post/edit?id=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("edit"));
+    }
+}
